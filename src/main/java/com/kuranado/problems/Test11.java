bbd0c6dbd9fbb50aec98bc6751ee274beb2cf18b
kuranado <jingxinling1024@163.com>
2020-03-24 12:52:16 +0800

    完成 37. 解数独

--- src/main/java/com/kuranado/problems/Test11.java
+++ src/main/java/com/kuranado/problems/Test11.java
@@ -10,6 +10,7 @@ package com.kuranado.problems;
 public class Test11 {
 
     public static void main(String[] args) {
+        // Test Sample 1
         char[][] board = new char[][]{
             {'5', '3', '.', '.', '7', '.', '.', '.', '.'},
             {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
@@ -20,17 +21,54 @@ public class Test11 {
             {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
             {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
             {'.', '.', '.', '.', '8', '.', '.', '7', '9'}};
+        // Answer
+        // 5  3  4  6  7  8  9  1  2
+        // 6  7  2  1  9  5  3  4  8
+        // 1  9  8  3  4  2  5  6  7
+        // 8  5  9  7  6  1  4  2  3
+        // 4  2  6  8  5  3  7  9  1
+        // 7  1  3  9  2  4  8  5  6
+        // 9  6  1  5  3  7  2  8  4
+        // 2  8  7  4  1  9  6  3  5
+        // 3  4  5  2  8  6  1  7  9
+
+        // Test Sample 2
+        //char[][] board = new char[][]{
+        //    {'.','.','9','7','4','8','.','.','.'},
+        //    {'7','.','.','.','.','.','.','.','.'},
+        //    {'.','2','.','1','.','9','.','.','.'},
+        //    {'.','.','7','.','.','.','2','4','.'},
+        //    {'.','6','4','.','1','.','5','9','.'},
+        //    {'.','9','8','.','.','.','3','.','.'},
+        //    {'.','.','.','8','.','3','.','2','.'},
+        //    {'.','.','.','.','.','.','.','.','6'},
+        //    {'.','.','.','2','7','5','9','.','.'}};
+        // Answer
+        // 5  1  9  7  4  8  6  3  2
+        // 7  8  3  6  5  2  4  1  9
+        // 4  2  6  1  3  9  8  7  5
+        // 3  5  7  9  8  6  2  4  1
+        // 2  6  4  3  1  7  5  9  8
+        // 1  9  8  5  2  4  3  6  7
+        // 9  7  5  8  6  3  1  2  4
+        // 8  3  2  4  9  1  7  5  6
+        // 6  4  1  2  7  5  9  8  3
+
         solveSudoku(board);
         printSudoku(board);
     }
 
-    public static void solveSudoku(char[][] board) {
+    private static void solveSudoku(char[][] board) {
         fun(board, false);
     }
 
-    static void fun(char[][] board, boolean solved) {
+    private static boolean fun(char[][] board, boolean solved) {
+        boolean s = solved;
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
+                if (solved) {
+                    return true;
+                }
                 if (board[i][j] == '.') {
                     int[] candidates = new int[10];
                     for (int m = 0; m < 9; m++) {
@@ -60,12 +98,12 @@ public class Test11 {
                             flag = true;
                             board[i][j] = (char) (m + 48);
                             if (i == 8 && j == 8) {
-                                solved = true;
+                                s = true;
                             }
-                            solveSudoku(board);
+                            s = fun(board, s);
                         }
-                        if (solved) {
-                            return;
+                        if (s) {
+                            return true;
                         }
                         if (candidates[m] == -1) {
                             flag = false;
@@ -73,17 +111,15 @@ public class Test11 {
                         }
                     }
                     if (!flag) {
-                        return;
+                        return false;
                     }
-                }
-                if (i == 8 && j == 8) {
-                    return;
                 }
             }
         }
+        return true;
     }
 
-    static void printSudoku(char[][] board) {
+    private static void printSudoku(char[][] board) {
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
                 System.out.print(board[i][j] + " ");
